a87baf0fdbda7f91f65c22598d80c668268d0ad2
Prasad <toster342@gmail.com>
2023-06-04 22:11:23 +0530

    first commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,241 @@
-import Image from 'next/image'
+"use client";
+import React, { useEffect, useState } from "react";
 
-export default function Home() {
+import Boxc from "./components/boxc/Boxc";
+import Loader from "./components/loader/load";
+
+const Home = () => {
+  const [isA, setA] = useState(true);
+  const [isSearch, setSearch] = useState(false);
+  const [isWord, setWord] = useState("");
+  const [isquery, setQuer] = useState("");
+  const [results, setResult] = useState([]);
+  const [Qid, setQid] = useState("");
+  const [pagen, setPage] = useState(1);
+  const [tresults, setResu] = useState("");
+  const [lengh, setLengh] = useState(1);
+  const [isLoading, setLoading] = useState(false);
+  const [apiL, setLimit] = useState(false);
+  const [noMatch, setMatch] = useState(false);
+  useEffect(() => {
+    if (isquery == "") {
+      var ele = document.getElementsByClassName("tex");
+      var elem = document.getElementsByClassName("radi");
+      elem[1].checked = false;
+      elem[0].checked = false;
+      ele[0].value = "";
+    }
+  }, [isquery]);
+
+  const setQu = () => {
+    if (Qid == "a") {
+      setQuer(
+        `https://api.github.com/search/users?q=${isWord}&page=${pagen}&per_page=10`
+      );
+      setResu(`https://api.github.com/search/users?q=${isWord}`);
+    } else {
+      setQuer(
+        `https://api.github.com/search/repositories?q=${isWord}&page=${pagen}&per_page=10`
+      );
+      setResu(`https://api.github.com/search/repositories?q=${isWord}`);
+    }
+  };
+
+  useEffect(() => {
+    if (tresults != "") {
+      fetch(tresults)
+        .then((r) => r.json())
+        .then((res) => {
+          if (res.total_count < 1000) {
+            setLengh(res.total_count);
+          } else {
+            setLengh(1000);
+          }
+          if (res.total_count == 0) {
+            setMatch(true);
+          }
+        });
+    }
+  }, [tresults]);
+
+  const setQuery = (sea) => {
+    if (isWord != "") {
+      setQid(sea.target.id);
+
+      if (sea.target.id == "a") {
+        setA(true);
+
+        setQuer(
+          `https://api.github.com/search/users?q=${isWord}&page=${pagen}&per_page=10`
+        );
+      } else if (sea.target.id == "b") {
+        setA(false);
+        setSearch(false);
+        setQuer(
+          `https://api.github.com/search/repositories?q=${isWord}&page=${pagen}&per_page=10`
+        );
+      }
+    } else {
+      alert("Type Keywords to search");
+    }
+  };
+
+  useEffect(() => {
+    if (isquery != "") {
+      setLoading(true);
+      setTimeout(() => {
+        fetch(isquery)
+          .then((r) => r.json())
+          .then((res) => {
+            if (res.message != null) {
+              setLimit(true);
+            }
+            setResult(res.items);
+            setLoading(false);
+          });
+      }, 1000);
+    }
+  }, [isquery]);
+
+  const setRes = () => {
+    if (isquery != "") {
+      setQu();
+    } else {
+      alert("select option");
+    }
+  };
+
+  const setSea = (code) => {
+    if (code.type == "keyup") {
+      setWord(code.target.value);
+
+      if (code.keyCode == 13) {
+        setRes();
+      }
+    } else if (code.type == "click") {
+      if (isWord != "" && isquery != "") {
+        setSearch(true);
+
+        setRes();
+      } else {
+        alert("Search/option empty");
+      }
+    }
+  };
+  useEffect(() => {}, [lengh]);
+  useEffect(() => {
+    if (isSearch) {
+      setRes();
+    }
+  }, [pagen]);
+
+  const Counter = (cla) => {
+    if (cla.target.className == "prev") {
+      if (pagen > 1) {
+        setPage(pagen - 1);
+      }
+    } else {
+      if (pagen < lengh / 10) {
+        setPage(pagen + 1);
+      }
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <div id="hcon">
+      <h1>Home Page</h1>
+      <input
+        className="tex"
+        onKeyUp={setSea}
+        type="text"
+        placeholder="Search"
+      ></input>
+      <br></br>
+      <br></br>
+      <label>Name</label>
+      <input
+        id="a"
+        onClick={setQuery}
+        name="same"
+        className="radi"
+        type="radio"
+        required
+      ></input>
+      <label>Project</label>{" "}
+      <input
+        id="b"
+        onClick={setQuery}
+        name="same"
+        className="radi"
+        type="radio"
+        required
+      ></input>
+      <br></br>
+      <br></br>
+      <button onClick={setSea}>Search</button>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      {isSearch && (
+        <>
+          <button className="prev" onClick={Counter}>
+            Prev
+          </button>
+          &nbsp;&nbsp;&nbsp;{pagen != 1 && <span className="page1">1</span>}
+          &ensp;{pagen <= 2 ? "" : <span className="pagel">{pagen - 1}</span>}
+          &ensp;{<span className="pagecur">{pagen}</span>} •••••{" "}
+          {lengh != pagen && <span className="pagel"> {lengh}</span>}
+          &nbsp;&nbsp;&nbsp;&nbsp;
+          <button className="next" onClick={Counter}>
+            Next
+          </button>
+        </>
+      )}
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      {isA && isSearch && !apiL ? (
+        isLoading ? (
+          <Loader />
+        ) : (
+          results.map((res) => (
+            <Boxc
+              name={res.login}
+              key={res.id}
+              link={res.html_url}
+              imgs={res.avatar_url}
             />
-          </a>
-        </div>
-      </div>
+          ))
+        )
+      ) : (
+        ""
+      )}
+      {!isA && isSearch && !apiL ? (
+        isLoading ? (
+          <Loader />
+        ) : (
+          results.map((res) => (
+            <Boxc
+              name={res.full_name}
+              key={res.id}
+              link={res.owner.html_url}
+              imgs={res.owner.avatar_url}
+              Project={res.html_url}
+              Desc={res.description}
+              Dat={res.updated_at}
+              homep={res.homepage}
+            />
+          ))
+        )
+      ) : (
+        ""
+      )}
+      {apiL && <h2>Api Limit Reached search after 10s</h2>}
+      {!isLoading && noMatch && <h2>No Match Found</h2>}
+    </div>
+  );
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Home;
